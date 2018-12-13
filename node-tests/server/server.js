c88752f8b0fadbf03a8aa495c9f2e93f96579974
marinaysn <marinaysn@gmail.com>
2018-12-13 00:23:39 -0500

    Added regionals to the tests

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,8 +3,26 @@ const express = require('express');
 
 let app = express();
 app.get('/', (req, res) => {
-    res.send('Hello world');
-})
+    res.status(404).send({
+        error: 'Page Not Found',
+        name: 'TODO App v1.0'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.send([{
+        name: 'Marina',
+        age: 42
+    },
+    {
+        name: 'Jane',
+        age: 37
+    },
+    {
+        name: 'Nadia',
+        age: 39
+    }]);
+});
 
 app.listen(3000);
 module.exports.app = app;
--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -1,1 +1,38 @@
 const request = require('supertest');
+let app = require('./server').app;
+let expect = require('expect');
+
+describe('Service Tests', () => {
+    
+    describe('#First Test', () => {
+        
+        it('should return 404 - Page Not Found', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page Not Found'
+                    });
+                })
+                .end(done);
+        })
+    })
+ 
+    describe('#Second Test', () => {
+        it('should return Jane object', (done) => {
+    
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Jane',
+                    age: 37})
+                })
+                .end(done);
+        }) 
+    })
+
+
+})
