87b90d84e943ee734160a8d43740c866370a2bc9
marinaysn <marinaysn@gmail.com>
2018-12-14 23:47:53 -0500

    clean up

--- node-tests/spies/app.js
+++ node-tests/spies/app.js
@@ -3,9 +3,4 @@ let db = require('./db.js');
 module.exports.handleSignup = (email, password) => {
     //Check if email exist
     db.saveUser({ email, password });
-
-    //Save the user to the database
-
-
-    // Send the welcome email
 }
